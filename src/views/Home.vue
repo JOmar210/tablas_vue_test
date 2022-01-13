78e339cefbfa0f89b4de21ffec071fa07819f654
JOmar210 <ocervantes@quickapps.mx>
2022-01-13 11:53:14 -0600

    Index

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <Index />
+</template>
+
+<script>
+  import Index from '../components/Index.vue'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Index,
+    },
+  }
+</script>
