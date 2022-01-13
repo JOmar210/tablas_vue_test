3de28575469314b9b8d9741ac754936b4ab0acb0
JOmar210 <ocervantes@quickapps.mx>
2022-01-13 17:36:29 -0600

    3Botones de filtros

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="main">
+      <Header/>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 export default {
   name: 'App',
-
+components: { Header},
   data: () => ({
     //
   }),
 };
 </script>
+<style>
+#app, .main{
+  background-color: rgb(198, 182, 131);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+      height="80px"
+    >
+        <v-img 
+        src="https://images.squarespace-cdn.com/content/v1/5d6c197f8a813e0001fd4ea7/1567373639160-CHZJP9CAPMPYYQNOBC2O/JJC-Full-Logo-Gold.png?format=1500w"
+        max-height="150"
+        max-width="250"
+        ></v-img>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
